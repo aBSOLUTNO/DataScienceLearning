ab48a0c17f2cc0e432b84771fc7792618b20601b
Konsta <konjakcoding@gmail.com>
2022-03-28 06:04:27 +0300

    Second commit

--- Python For Data Science/datatestfile.py
+++ Python For Data Science/datatestfile.py
@@ -1,5 +1,96 @@
-import enum
+
+from gettext import find
 from random import random
+import pandas as pd
+
+#Image process stuff.
+
+from skimage.io import imread
+from skimage.transform import resize
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+
+# Fucking pymongo
+
+import 
+
+
+
+# Getting the file from net.
+example_file = ("http://upload.wikimedia.org/" + 
+"wikipedia/commons/7/7d/Dog_face.png")
+
+#Stores the image as gray in variable. And then shows it frm matplotlib.
+image = imread(example_file, as_gray=True)
+
+plt.imshow(image, cmap=cm.gray)
+plt.show()
+
+#Shows the data from image.
+print("data type: %s, shape: %s" %(type(image), image.shape))
+
+#Manipulating the image array. Cropping is essential so images are the same size.
+
+image2 = image[5:70,0:70]
+plt.imshow(image2, cmap=cm.gray)
+plt.show()
+
+#Resize image to specific size for anlysis.
+
+image3 = resize(image2, (30,30))
+plt.imshow(image3, cmap=cm.gray)
+plt.show()
+
+  
+#Multiple images.
+
+fig = plt.figure()
+ax1 = fig.add_subplot(2,2,1)
+ax1.imshow(image, cmap=cm.gray)
+ax2 = fig.add_subplot(2,2,2)
+ax2.imshow(image2, cmap=cm.gray)
+ax3 = fig.add_subplot(2,2,3)
+ax3.imshow(image3, cmap=cm.gray)
+plt.show()
+
+#Flattenig images.
+
+image_row = image3.flatten()
+print(("data type: %s. shape: %s" % (type(image_row), image_row.shape)))
+
+
+
+
+#Reading CSV formats. Reads ages from titanic.csv
+
+titanic = pd.read_csv("titanic.csv")
+x = titanic[['age']]
+print(x)
+(type(image_row), image_row.shape)
+
+
+
+
+#Reading CSV formats. Reads ages from titanic.csv
+
+titanic = pd.read_csv("titanic.csv")
+x = titanic[['age']]
+print(x)
+
+#Reading Excel files.
+
+xls = pd.ExcelFile("Values.xls")
+trig_values = xls.parse('Sheet1', index_col=None, na_values=['NA'])
+
+print(trig_values)
+
+
+#Reading Excel files.
+
+xls = pd.ExcelFile("Values.xls")
+trig_values = xls.parse('Sheet1', index_col=None, na_values=['NA'])
+
+print(trig_values)
 
 
 #Loading all of the data in memory.
